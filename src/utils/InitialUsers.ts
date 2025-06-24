2f5339eae70f3317996a94becc902813ea62c349
Aldas Kasputis <80693733+aldask@users.noreply.github.com>
2025-06-24 17:02:47 +0300

    Initial users defined

---
+++ src/utils/InitialUsers.ts
@@ -0,0 +1,31 @@
+import type { User } from "../types/User";
+
+export const InitialUsers: User[] = [
+  {
+    id: "1",
+    f_name: "Jonas",
+    l_name: "Donas",
+    email: "jonas.donas@mail.com",
+    age: 30,
+    gender: "Male",
+    role: "Accountant",
+  },
+  {
+    id: "2",
+    f_name: "Janė",
+    l_name: "Janauske",
+    email: "jane.janauske@mail.com",
+    age: 25,
+    gender: "Female",
+    role: "Manager",
+  },
+  {
+    id: "3",
+    f_name: "Tomas",
+    l_name: "Komas",
+    email: "tomas.komas@mail.com",
+    age: 35,
+    gender: "Male",
+    role: "Developer",
+  },
+];
