f261e7c72bfd93e91beaa8ab6722240d629acd30
Aldas Kasputis <80693733+aldask@users.noreply.github.com>
2025-06-25 16:22:02 +0300

    Chuck phrase page done

--- src/pages/ChuckPage.tsx
+++ src/pages/ChuckPage.tsx
@@ -1,30 +1,38 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import type { ChuckResponse } from "../types/User";
+import useChuck from "../hooks/useChuck";
 
 const ChuckPage = () => {
-  const [chuckSays, setChuckSays] = useState<ChuckResponse>();
-  const [lastFetched, setLastFetched] = useState<Date>();
-
-  const fetchChuckPhrase = async () => {
-    try {
-      const response = await axios.get(
-        "https://api.chucknorris.io/jokes/random?category=dev"
-      );
-      setChuckSays(response.data);
-      setLastFetched(new Date());
-    } catch (error) {
-      console.error("Error from Chuck (Which is impossible).");
-    }
-  };
-
-  useEffect(() => {
-    fetchChuckPhrase();
-  }, []);
+  const { chuckSays, lastFetched, fetchChuckPhrase } = useChuck();
 
   return (
-    <div className="min-h-screen bg-gray-100 py-10 px-4">
-      <p>Chuck says: {chuckSays?.value}</p>
+    <div className="min-h-screen flex flex-col justify-center items-center py-12 px-4 sm:px-10">
+      <div className="relative max-w-4xl w-full bg-white rounded-2xl shadow-md border border-amber-400 p-10">
+        <div className="flex items-center gap-5 mb-8">
+          <img
+            src={chuckSays?.icon_url}
+            alt="Chuck Norris"
+            className="w-20 h-20 rounded-full border-4 border-amber-600"
+          />
+          <h2 className="text-2xl font-semibold text-amber-600">
+            Chuck Wisdom Says:
+          </h2>
+        </div>
+
+        <blockquote className="text-xl sm:text-2xl italic leading-relaxed text-gray-700 mb-8">
+          “{chuckSays?.value}”
+        </blockquote>
+        {lastFetched && (
+          <p className="text-sm text-gray-500 mb-8">
+            Last updated: {lastFetched.toLocaleString()}
+          </p>
+        )}
+        <button
+          onClick={fetchChuckPhrase}
+          className="absolute top-6 right-6 bg-gray-200 text-gray-800 rounded-full p-1 shadow-md transition duration-300 transform hover:-translate-y-1 active:scale-95"
+          title="Fetch new quote"
+        >
+          🔁
+        </button>
+      </div>
     </div>
   );
 };
